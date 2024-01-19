20581782d1aa6bd1a87cfae7e4dc4a0eba32856f
sid <sid@portuus.de>
2024-01-19 13:48:12 +0100

    patch: xresources

--- config.def.h
+++ config.def.h
@@ -5,20 +5,35 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                 /* minimum width when centered */
 
+static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
+static char font[] = "monospace:size=10";
 static const char *fonts[] = {
-	"monospace:size=10"
+    font,
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-	[SchemeBorder] = { "#cccccc", NULL },
+
+static char normfgcolor[] = "#bbbbbb";
+static char normbgcolor[] = "#222222";
+static char selfgcolor[]  = "#eeeeee";
+static char selbgcolor[]  = "#005577";
+static char outfgcolor[]  = "#000000";
+static char outbgcolor[]  = "#00ffff";
+static char normhighlfgcolor[] = "#000000";
+static char normhighlbgcolor[] = "#00ffff";
+static char selbordercolor[] = "#00ffff";
+static char selhighlfgcolor[]  = "#000000";
+static char selhighlbgcolor[]  = "#00ffff";
+static char *colors[SchemeLast][2] = {
+	/*                        fg                bg       */
+   	[SchemeNorm]          = { normfgcolor,      normbgcolor },
+	[SchemeSel]           = { selfgcolor,       selbgcolor },
+	[SchemeOut]           = { outfgcolor,       outbgcolor },
+    [SchemeBorder]        = { selbordercolor,   NULL },
+	[SchemeNormHighlight] = { normhighlfgcolor, normhighlbgcolor },
+	[SchemeSelHighlight]  = { selhighlfgcolor,  selhighlbgcolor },
 };
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
@@ -30,3 +45,22 @@ static const char worddelimiters[] = " ";
 
 /* Size of the window border */
 static unsigned int border_width = 2;
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+	{ "font",             STRING, &font },
+	{ "normfgcolor",      STRING, &normfgcolor },
+	{ "normbgcolor",      STRING, &normbgcolor },
+	{ "selfgcolor",       STRING, &selfgcolor },
+	{ "selbgcolor",       STRING, &selbgcolor },
+    { "outfgcolor",       STRING, &outfgcolor },
+    { "outbgcolor",       STRING, &outbgcolor },
+    { "normhighlfgcolor", STRING, &normhighlfgcolor },
+    { "normhighlbgcolor", STRING, &normhighlbgcolor },
+    { "selbordercolor",   STRING, &selbordercolor },
+    { "selhighlfgcolor",  STRING, &selhighlfgcolor },
+    { "selhighlbgcolor",  STRING, &selhighlbgcolor },
+	{ "prompt",           STRING, &prompt },
+};
--- config.h
+++ config.h
@@ -1,26 +1,36 @@
 /* Default settings; can be overriden by command line. */
 
-#include "colors.h"
-
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                 /* minimum width when centered */
 
-static const char *prompt = NULL;           /* -p  option; prompt to the left of input field */
+static char *prompt = NULL;                 /* -p  option; prompt to the left of input field */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
+static char font[] = "Hack:size=14";
 static const char *fonts[] = {
-	"Hack:size=14"
+    font,
 };
 
-static const char *colors[SchemeLast][2] = {
-	/*                        fg          bg         */
-	[SchemeNorm]          = { d_white,    background },
-	[SchemeSel]           = { d_white,    d_black },
-	[SchemeOut]           = { background, d_blue },
-    [SchemeBorder]        = { d_blue,     NULL },
-	[SchemeNormHighlight] = { d_blue,     background },
-	[SchemeSelHighlight]  = { d_blue,     d_black },
+static char normfgcolor[] = "#bbbbbb";
+static char normbgcolor[] = "#222222";
+static char selfgcolor[] = "#eeeeee";
+static char selbgcolor[] = "#005577";
+static char outfgcolor[] = "#000000";
+static char outbgcolor[] = "#00ffff";
+static char normhighlfgcolor[] = "#000000";
+static char normhighlbgcolor[] = "#00ffff";
+static char selbordercolor[] = "#00ffff";
+static char selhighlfgcolor[] = "#000000";
+static char selhighlbgcolor[] = "#00ffff";
+static char *colors[SchemeLast][2] = {
+	/*                        fg                bg       */
+   	[SchemeNorm]          = { normfgcolor,      normbgcolor },
+	[SchemeSel]           = { selfgcolor,       selbgcolor },
+	[SchemeOut]           = { outfgcolor,       outbgcolor },
+    [SchemeBorder]        = { selbordercolor,   NULL },
+	[SchemeNormHighlight] = { normhighlfgcolor, normhighlbgcolor },
+	[SchemeSelHighlight]  = { selhighlfgcolor,  selhighlbgcolor },
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
@@ -31,3 +41,22 @@ static const char worddelimiters[] = " ";
 
 /* Size of the window border */
 static unsigned int border_width = 2;
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+	{ "font",             STRING, &font },
+	{ "normfgcolor",      STRING, &normfgcolor },
+	{ "normbgcolor",      STRING, &normbgcolor },
+	{ "selfgcolor",       STRING, &selfgcolor },
+	{ "selbgcolor",       STRING, &selbgcolor },
+    { "outfgcolor",       STRING, &outfgcolor },
+    { "outbgcolor",       STRING, &outbgcolor },
+    { "normhighlfgcolor", STRING, &normhighlfgcolor },
+    { "normhighlbgcolor", STRING, &normhighlbgcolor },
+    { "selbordercolor",   STRING, &selbordercolor },
+    { "selhighlfgcolor",  STRING, &selhighlfgcolor },
+    { "selhighlbgcolor",  STRING, &selhighlbgcolor },
+	{ "prompt",           STRING, &prompt },
+};
